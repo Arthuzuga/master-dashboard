1480984ba84a316f2b12b322089cf2f9f3b92d3e
Joao Tavares <joao.arthur.tavares@gmail.com>
2020-03-30 23:45:13 -0300

    fix(HTTPS): change API URL to HTTPS

--- src/services/getEquipments.js
+++ src/services/getEquipments.js
@@ -1,9 +1,9 @@
 export const getEquipmentsCategory = async () => {
- const res = await fetch("http://www.dnd5eapi.co/api/equipment-categories/");
+ const res = await fetch("https://www.dnd5eapi.co/api/equipment-categories/");
  return res.json();
 };
 
 export const getEquipments = async (url) => {
- const res = await fetch(`http://www.dnd5eapi.co${url}`);
+ const res = await fetch(`https://www.dnd5eapi.co${url}`);
  return res.json();
 };
