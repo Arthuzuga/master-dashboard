04f178b96373fa287d2e77697e80d353e2074755
Joao Tavares <joao.arthur.tavares@gmail.com>
2020-05-22 12:16:46 -0300

    feat(persist data on localstorage)

--- src/redux/reducers/selectCampaign_reducer.js
+++ src/redux/reducers/selectCampaign_reducer.js
@@ -3,6 +3,7 @@ import { SELECT_CAMPAIGN } from "../actions/actionsType";
 export default (state = {}, action) => {
  switch (action.type) {
   case SELECT_CAMPAIGN:
+    localStorage.setItem("selectedCampaign", JSON.stringify(action.data))
    return action.data;
   default:
    return state;
--- src/redux/reducers/selectSession_reducer.js
+++ src/redux/reducers/selectSession_reducer.js
@@ -3,6 +3,7 @@ import { SELECT_SESSION } from "../actions/actionsType";
 export default (state = {}, action) => {
  switch (action.type) {
   case SELECT_SESSION:
+    localStorage.setItem('selectedSession', JSON.stringify(action.data))
    return action.data;
   default:
    return state;
